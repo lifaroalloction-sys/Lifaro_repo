321272f9ed3f108fe71175df74b460461968dc38
M.VINOTHKUMAR <vinothkumar6381650856@gmail.com>
2026-09-26 12:51:04 +0530

    Client: use Unsplash images, remote avatar and add --legacy-peer-deps for Actions

--- client/src/components/PostGrid.jsx
+++ client/src/components/PostGrid.jsx
@@ -10,8 +10,12 @@ function PostTile({src, alt}){
 
 export default function PostGrid(){
   const sample = [
-    '/images/p1.jpg','/images/p2.jpg','/images/p3.jpg',
-    '/images/p4.jpg','/images/p5.jpg','/images/p6.jpg'
+    'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=1200&q=80',
+    'https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=1200&q=80',
+    'https://images.unsplash.com/photo-1470770903676-69b98201ea1c?w=1200&q=80',
+    'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1200&q=80',
+    'https://images.unsplash.com/photo-1491553895911-0055eca6402d?w=1200&q=80',
+    'https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=1200&q=80'
   ]
   return (
     <section className="post-grid">
